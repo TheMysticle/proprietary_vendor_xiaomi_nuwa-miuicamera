2c33895e0527c55517eb5cbcdb4a698c90c3ae96
Lunark :3 <jvillaltamatos3007@gmail.com>
2026-06-15 20:52:04 -0500

    vermeer-miuicamera: Import from OS3.0.301.0.WNKMIXM
    Change-Id: I89f668773ab9c85fdcd335e273a5c6bce6a084c2

--- vermeer-miuicamera-vendor.mk
+++ vermeer-miuicamera-vendor.mk
@@ -4,3 +4,10 @@
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/xiaomi/vermeer-miuicamera
+
+PRODUCT_PACKAGES += \
+    libcamera_algoup_jni.xiaomi \
+    libcamera_mianode_jni.xiaomi \
+    libmicampostproc_client \
+    vendor.xiaomi.hardware.campostproc@1.0_system \
+    MiuiCamera
